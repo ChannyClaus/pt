d7ac0f735f66c5645b54e0f3436a978fab4352a0
Chan Kang <chankang17@gmail.com>
2024-07-18 23:33:19 -0400

    remove comment

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,13 @@
-// This is a comment, and is ignored by the compiler.
-// You can test this code by clicking the "Run" button over there ->
-// or if you prefer to use your keyboard, you can use the "Ctrl + Enter"
-// shortcut.
-
-// This code is editable, feel free to hack it!
-// You can always return to the original code by clicking the "Reset" button ->
-
 use ruff_python_parser;
-use std::{env, fs};
+use std::env;
 
 // This is the main function.
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let test_root = &args[1];
-    println!("Test root: {}", test_root);
-    let paths = fs::read_dir(test_root).unwrap();
-
-    for path in paths {
-        let parsed = ruff_python_parser::parse(
-            path.unwrap().path().to_str().unwrap(),
-            ruff_python_parser::Mode::Module,
-        );
-        println!("{:#?}", parsed);
-    }
+    let test_file = &args[1];
+    println!("test_file: {}", test_file);
+    let parsed = ruff_python_parser::parse(&test_file, ruff_python_parser::Mode::Module);
+    println!("{:#?}", parsed);
     // Statements here are executed when the compiled binary is called.
 
     // Print text to the console.
