21b848a5c7dc12511a6210dd7cc3fbebd0c8caf1
Chan Kang <chankang17@gmail.com>
2024-07-21 23:21:41 -0400

    add all for now

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,46 @@
+use crate::ruff_python_ast::Stmt::ClassDef;
+use crate::ruff_python_ast::Stmt::FunctionDef;
+use ruff_python_ast::{self};
 use ruff_python_parser;
-use std::env;
+use std::{env, fs};
 
-// This is the main function.
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let test_file = &args[1];
-    println!("test_file: {}", test_file);
-    let parsed = ruff_python_parser::parse(&test_file, ruff_python_parser::Mode::Module);
-    println!("{:#?}", parsed);
-    // Statements here are executed when the compiled binary is called.
+    let path = &args[1];
+    let source = fs::read_to_string(path).unwrap();
 
-    // Print text to the console.
-    println!("Hello World!");
+    println!("test_file: {}", path);
+    let mut parsed = ruff_python_parser::parse(&source, ruff_python_parser::Mode::Module).unwrap();
+    println!("parsed: {:#?}", parsed);
+    match parsed.syntax {
+        ruff_python_ast::Mod::Module(ref mut module) => {
+            module.body = module.body[..1].to_vec();
+            for stmt in &module.body {
+                match stmt {
+                    FunctionDef(stmt) => {
+                        println!("stmt: {:#?}", stmt.name.id.to_string());
+                    }
+                    ClassDef(stmt) => {
+                        println!("stmt: {:#?}", stmt.name.id.to_string());
+                    }
+                    _ => continue,
+                }
+            }
+        }
+        _ => {
+            println!("not module");
+        }
+    }
+
+    let comment_ranges = ruff_python_trivia::CommentRanges::from(parsed.tokens());
+    let formatted = ruff_python_formatter::format_module_ast(
+        &parsed,
+        &comment_ranges,
+        source.as_str(),
+        ruff_python_formatter::PyFormatOptions::default(),
+    );
+    println!(
+        "formatted.print(): {}",
+        formatted.unwrap().print().unwrap().as_code()
+    );
 }
---
+++ test.py
@@ -0,0 +1,18 @@
+def test_chan1():
+    assert False
+    pass
+
+
+@pt.mark.skip
+def test_chan2():
+
+
+
+
+
+
+
+    pass
+
+
+test_chan1()
