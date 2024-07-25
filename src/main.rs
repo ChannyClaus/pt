cacb27b54a0d4437e6e02f7912929f6822d0f3c8
chan <chan.kang@enablemedicine.com>
2024-07-24 22:51:18 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
 use crate::ruff_python_ast::Stmt::ClassDef;
 use crate::ruff_python_ast::Stmt::FunctionDef;
+use ruff_formatter::SourceCode;
 use ruff_python_ast::{self};
+use ruff_python_formatter::AsFormat;
 use ruff_python_parser;
+use ruff_source_file::Locator;
 use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let path = &args[1];
-    let source = fs::read_to_string(path).unwrap();
+    let binding = fs::read_to_string(path).unwrap();
+    let source = binding.as_str();
 
     println!("test_file: {}", path);
-    let mut parsed = ruff_python_parser::parse(&source, ruff_python_parser::Mode::Module).unwrap();
-    println!("parsed: {:#?}", parsed);
-    match parsed.syntax {
-        ruff_python_ast::Mod::Module(ref mut module) => {
-            module.body = module.body[..1].to_vec();
+    let parsed = ruff_python_parser::parse(&source, ruff_python_parser::Mode::Module).unwrap();
+    // println!("parsed: {:#?}", parsed);
+    match parsed.syntax() {
+        ruff_python_ast::Mod::Module(module) => {
+            // module.body = module.body[..1].to_vec();
+            let source_code = SourceCode::new(source);
+            //let comments = Comments::from_ast(parsed.syntax(), source_code, comment_ranges);
+            let locator = Locator::new(source);
+
+            // let formatted = format!(
+            //   PyFormatContext::new(options, locator.contents(), comments, parsed.tokens()), [module.format()],
+            // )
             for stmt in &module.body {
                 match stmt {
                     FunctionDef(stmt) => {
@@ -32,11 +43,11 @@ fn main() {
         }
     }
 
-    let comment_ranges = ruff_python_trivia::CommentRanges::from(parsed.tokens());
+    let comment_ranges = ruff_python_trivia::CommentRanges::new(vec![]);
     let formatted = ruff_python_formatter::format_module_ast(
         &parsed,
         &comment_ranges,
-        source.as_str(),
+        source,
         ruff_python_formatter::PyFormatOptions::default(),
     );
     println!(
--- test.py
+++ test.py
@@ -2,13 +2,13 @@ def test_chan1():
     assert False
     pass
 
-
+# asdfadsfs
 @pt.mark.skip
 def test_chan2():
 
 
 
-
+# asdf
 
 
 
