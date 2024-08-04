927bd83e0cf740b9a4ffce475b7075896acb29e7
Chan Kang <chankang17@gmail.com>
2024-08-04 07:38:10 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,124 @@
 use pyo3::prelude::*;
 mod normalizer;
-use ruff_python_ast::{str::Quote, ExprAttribute, StmtFunctionDef};
+use ruff_python_ast::{str::Quote, Expr, ExprAttribute, Stmt, StmtFunctionDef};
 use ruff_python_codegen::{stylist::Indentation, Generator};
-use ruff_python_parser::{self};
+use ruff_python_parser::{self, parse};
 use ruff_source_file::LineEnding;
-use std::{env, fs};
+use std::{
+    env,
+    fs::{self, metadata},
+};
 
 #[derive(Debug)]
-pub struct TestFile {
+pub struct Fixture {
     pub name: String,
-    pub fixtures: Vec<String>,
+}
+
+#[derive(Debug)]
+pub struct Package {
+    pub source: String,
+    pub modules: Vec<Module>,
+    pub subpackages: Vec<Package>,
+}
+
+impl Package {
+    pub fn from_dir(path: &str) -> Self {
+        let paths = fs::read_dir(path).unwrap();
+
+        let mut subpackages = vec![];
+        let mut modules = vec![];
+
+        for path in paths {
+            let p = path.unwrap().path();
+            let pstr = p.to_str().unwrap().to_string();
+            if p.is_dir() && !pstr.contains("/.") {
+                subpackages.push(Package::from_dir(p.to_str().unwrap()));
+                continue;
+            }
+
+            if pstr.starts_with("test") && pstr.ends_with(".py") {
+                modules.push(Module::from_file(p.to_str().unwrap()));
+                continue;
+            }
+        }
+
+        Self {
+            source: path.to_string(),
+            modules,
+            subpackages,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Module {
+    pub path: String,
+    pub fixtures: Vec<Fixture>,
     pub tests: Vec<String>,
 }
 
-fn parse_testfile(path: &str) -> TestFile {
-    let source = fs::read_to_string(path).unwrap();
-    let parsed = ruff_python_parser::parse(&source, ruff_python_parser::Mode::Module).unwrap();
-    let syntax = parsed.into_syntax();
+impl Module {
+    pub fn from_file(path: &str) -> Self {
+        let source = fs::read_to_string(path).unwrap();
+        let parsed = parse(&source, ruff_python_parser::Mode::Module).unwrap();
+        let syntax = parsed.into_syntax();
 
-    let mut tests = vec![];
-    let mut fixtures = vec![];
-    for stmt in syntax.as_module().unwrap().body.iter() {
-        if let ruff_python_ast::Stmt::FunctionDef(StmtFunctionDef {
-            name,
-            decorator_list,
-            ..
-        }) = stmt
-        {
-            if name.starts_with("test") {
-                tests.push(name.to_string());
-                continue;
-            }
-            for decorator in decorator_list.iter() {
-                if let ruff_python_ast::Expr::Attribute(ExprAttribute { value, attr, .. }) =
-                    &decorator.expression
-                {
-                    if let ruff_python_ast::Expr::Name(expr_name) = &*value.clone() {
-                        if expr_name.id.to_string() == "ptst" && attr.id.to_string() == "fixture" {
-                            fixtures.push(name.to_string());
+        let mut tests = vec![];
+        let mut fixtures = vec![];
+        for stmt in syntax.as_module().unwrap().body.iter() {
+            if let Stmt::FunctionDef(StmtFunctionDef {
+                name,
+                decorator_list,
+                ..
+            }) = stmt
+            {
+                if name.starts_with("test") {
+                    tests.push(name.to_string());
+                    continue;
+                }
+                for decorator in decorator_list.iter() {
+                    if let Expr::Attribute(ExprAttribute { value, attr, .. }) =
+                        &decorator.expression
+                    {
+                        if let Expr::Name(expr_name) = &*value.clone() {
+                            if expr_name.id.to_string() == "ptst"
+                                && attr.id.to_string() == "fixture"
+                            {
+                                println!("decorator: {:#?}", decorator);
+                                fixtures.push(Fixture {
+                                    name: name.to_string(),
+                                })
+                            }
                         }
                     }
                 }
             }
         }
-    }
 
-    TestFile {
-        name: path.to_string(),
-        fixtures,
-        tests,
+        Self {
+            path: path.to_string(),
+            fixtures,
+            tests,
+        }
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if metadata(&args[1].clone()).unwrap().is_file() {
+        let module = Module::from_file(&args[1].clone());
+        // println!("module: {:#?}", module);
+        return;
+    }
+    let package = Package::from_dir(&args[1].clone());
+    // println!("package: {:#?}", package);
     // let paths = get_paths(args[1].clone()).unwrap();
-    let paths = fs::read_dir(args[1].clone())
-        .unwrap()
-        .into_iter()
-        .map(|entry| parse_testfile(entry.unwrap().path().to_str().unwrap()))
-        .collect::<Vec<_>>();
-    println!("{:#?}", paths);
+    // let paths = fs::read_dir(args[1].clone())
+    //     .unwrap()
+    //     .into_iter()
+    //     .map(|entry| Module::from_file(entry.unwrap().path().to_str().unwrap()))
+    //     .collect::<Vec<_>>();
+    // println!("{:#?}", paths);
 
     // TODO:
     // 0. validate the existing test files (fixtures being required all exist, etc)
